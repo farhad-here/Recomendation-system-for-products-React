7108068a28f0cd30a590c0cd14bf94f33e0e08eb
Farhad_Ghaherdoost <farhadghaher@gmail.com>
2026-05-14 04:28:31 +0330

    Initial project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import ProductDetails from './components/ProductDetails/ProductDetails';
+import Cart from './components/Cart/Cart';
+
+const App = () => {
+  return(
+    <>
+      <Router>
+        <Routes>
+          {/* home */}
+          <Route path='/' element={<Home />} />
+          {/* product detail */}
+          <Route path='/product/:id' element={<ProductDetails />} />
+          <Route path='/cart' element={<Cart />} />
+          
+        </Routes>
+      </Router>
+    </>
+  );
+
+
+}
+
+export default App;
