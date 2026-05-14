9d473d8dd5db8ced3f60e282967c7d0d1261588a
Farhad_Ghaherdoost <farhadghaher@gmail.com>
2026-05-14 19:50:10 +0330

    Fix bug Cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,32 +1,30 @@
 import { useState } from "react";
 import styles from './Cart.module.css';
 
+const Cart = () => {
+    const [cart, setCart] = useState(
+        JSON.parse(localStorage.getItem('cart')) || []
+    );
 
-const Cart = () =>{
-       const [cart, setCart] = useState(
-              JSON.parse(localStorage.getItem('cart'))||'[]',
-       );
+    const removeItem = (id) => {
+        const newCart = cart.filter((p) => p.id !== id);
+        setCart(newCart);
+        localStorage.setItem('cart', JSON.stringify(newCart));
+    };
 
-       const removeItem = (id) => {
-              const newCart = cart.filter((p)=>p.id != id);
-              setCart(newCart);
-              localStorage.setItem('cart',JSON.stringify(newCart));
-
-       };
-
-       return(
-              <div className={styles.cart}>
-                     <h2>Cart</h2>
-                     {cart.map((item)=>{
-                            <div key={item.id} className={styles.item}>
-                                   <img src={item.image} alt={item.tite} />
-                                   <p>{item.title}</p>
-                                   <button onClick={()=>removeItem(item.id)}>Remove</button>
-                            </div>
-                     })}
-              </div>
-       )
-}
+    return (
+        <div className={styles.cart}>
+            <h2>Cart</h2>
+            {cart.map((item) => (
+                <div key={item.id} className={styles.item}>
+                    <img src={item.image} alt={item.title} />
+                    <p>{item.title}</p>
+                    <button onClick={() => removeItem(item.id)}>Remove</button>
+                </div>
+            ))}
+        </div>
+    );
+};
 
 
 
