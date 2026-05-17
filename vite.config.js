803800a9cc668ac5e78d721985447c058e3c7f31
Farhad_Ghaherdoost <farhadghaher@gmail.com>
2026-05-17 20:24:47 +0330

    Add docs

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Recomendation-system-for-products-React/",
+  build: {
+    outDir: "docs"
+  }
 })
