f18c723a89632c449d46f86941ab2837bf444e3b
Farhad Ghaherdoost (Fred) <88395627+farhad-here@users.noreply.github.com>
2026-05-18 18:40:55 +0330

    Change Router to HashRouter in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home.jsx';
 import { ProductDetails } from './components/ProductDetails/ProductDetails';
 import { Cart } from './components/Cart/Cart';
